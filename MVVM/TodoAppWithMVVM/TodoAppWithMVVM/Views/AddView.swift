b59d1ad50bac5a26bdaee3ef35f248fcf5d9ecf4
Kim TaeSung <fopc4131@gmail.com>
2024-05-12 13:24:12 +0900

    Todo App With MVVM  Example - Model, ViewModel Add - 2

--- MVVM/TodoAppWithMVVM/TodoAppWithMVVM/Views/AddView.swift
+++ MVVM/TodoAppWithMVVM/TodoAppWithMVVM/Views/AddView.swift
@@ -11,7 +11,12 @@ import SwiftUI
 
 struct AddView: View {
     
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var listViewModel: ListViewModel
     @State var textFieldText: String = ""
+    
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
     
     var body: some View {
         ScrollView {
@@ -23,7 +28,7 @@ struct AddView: View {
                     .cornerRadius(10)
                 
                 Button(action: {
-                    
+                    saveButtonPressed()
                 }, label: {
                     Text("Save".uppercased())
                         .foregroundColor(.white)
@@ -37,11 +42,36 @@ struct AddView: View {
             .padding(16)
         }
         .navigationTitle("Add an Item 🖊️")
+        .alert(isPresented: $showAlert, content: {
+            getAlert()
+        })
+    }
+    
+    func saveButtonPressed() {
+        if textIsApproprite() {
+            listViewModel.addItem(title: textFieldText)
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    func textIsApproprite() -> Bool {
+        if textFieldText.count < 3 {
+            alertTitle = "Your new todo item must be at least 3 characters long!!!!"
+            showAlert.toggle()
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    func getAlert() -> Alert {
+        return Alert(title: Text(alertTitle))
     }
 }
 
 #Preview {
     NavigationView {
         AddView()
+            .environmentObject(ListViewModel())
     }
 }
