40e202b65498e48d27d1631fc88c65ff12f7b1f0
Kim TaeSung <fopc4131@gmail.com>
2024-05-14 23:23:39 +0900

    UserDefault, NoItems View Add

---
+++ MVVM/TodoAppWithMVVM/TodoAppWithMVVM/Views/NoItemsView.swift
@@ -0,0 +1,77 @@
+//
+//  NoItemsView.swift
+//  TodoAppWithMVVM
+//
+//  Created by 김태성 on 5/14/24.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    
+    @State var animate: Bool = false
+    let secondaryAccentColr = Color.secondsAccent
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 20) {
+                Text("There are no items!")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                
+                Text("Are you productive person? I think you should click the add button and add a bunch of items to your todo list!")
+                    .padding(.bottom, 20)
+                
+                NavigationLink(
+                    destination: AddView(),
+                    label: {
+                        Text("Add Someting")
+                            .foregroundStyle(.white)
+                            .font(.headline)
+                            .frame(height: 55)
+                            .frame(maxWidth: .infinity)
+                            .background(animate ? secondaryAccentColr : Color.accentColor)
+                            .cornerRadius(10)
+                            
+                    })
+                .padding(.horizontal, animate ? 30 : 50)
+                .shadow(
+                    color: animate ? secondaryAccentColr.opacity(0.7) :
+                        Color.accentColor.opacity(0.7),
+                    radius: animate ? 30: 10,
+                    x: 0,
+                    y: animate ? 50: 30
+                )
+                .scaleEffect(animate ? 1.1 : 1.0)
+                .offset(y: animate ? -7 : 0)
+            }
+            .multilineTextAlignment(.center)
+            .padding(40)
+            .onAppear(perform: addAnimation)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+    
+    func addAnimation() {
+        guard !animate else {
+            return
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 2.0)
+                    .repeatForever()
+            ) {
+                animate.toggle()
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationView {
+        NoItemsView()
+            .navigationTitle("Title")
+    }
+}
