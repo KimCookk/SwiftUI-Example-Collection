9b8c9153f5b7f2276c38a1b595b93d305c1a9ba3
Kim TaeSung <fopc4131@gmail.com>
2024-05-18 22:35:10 +0900

    Pomodoro Timer - 2

--- PomodoroTimer/PomodoroTimer/퍋ㅈ/HomeView.swift
+++ PomodoroTimer/PomodoroTimer/퍋ㅈ/HomeView.swift
@@ -101,6 +101,19 @@ struct HomeView: View {
             Color("BG")
                 .ignoresSafeArea()
         }
+        .overlay(content: {
+            ZStack {
+                Color.black
+                    .opacity(pomodoroModel.addNewTimer ? 0.25 : 0)
+                    .onTapGesture {
+                        pomodoroModel.addNewTimer = false
+                    }
+                
+                NewTimeView()
+                    .frame(maxHeight: .infinity, alignment: .bottom)
+                    .offset(y: pomodoroModel.addNewTimer ? 0 : 400)
+            }
+        })
         .preferredColorScheme(.dark)
     }
     
@@ -112,6 +125,61 @@ struct HomeView: View {
                 .font(.title2.bold())
                 .foregroundColor(.white)
                 .padding(.top, 10)
+            
+            HStack(spacing: 15) {
+                Text("\(pomodoroModel.hour) hr")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white.opacity(0.3))
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 12)
+                    .background() {
+                        Capsule()
+                            .fill(.white.opacity(0.3))
+                    }
+                
+                Text("\(pomodoroModel.hour) min")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white.opacity(0.3))
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 12)
+                    .background() {
+                        Capsule()
+                            .fill(.white.opacity(0.3))
+                    }
+                
+                Text("\(pomodoroModel.hour) sec")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white.opacity(0.3))
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 12)
+                    .background() {
+                        Capsule()
+                            .fill(.white.opacity(0.3))
+                    }
+            }
+            .padding(.top, 20)
+            
+            Button {
+                
+            } label: {
+                Text("Save")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding(.vertical)
+                    .padding(.horizontal, 100)
+                    .background{
+                        Capsule()
+                            .fill(Color("Purple"))
+                    }
+            }
+            .disabled(pomodoroModel.second == 0)
+            .opacity(pomodoroModel.second == 0 ? 0.5 : 1)
+            .padding(.top)
+
         }
         .padding()
         .frame(maxWidth: .infinity)
@@ -119,6 +187,13 @@ struct HomeView: View {
             RoundedRectangle(cornerRadius: 10, style: .continuous)
                 .fill(Color("BG"))
                 .ignoresSafeArea()
+        }
+    }
+    
+    // MARK: Reusable Context Menu Options
+    func ContextMenuOptions(maxValue: Int, hint: String, onClick: @escaping (Int)->()) {
+        ForEach(0...maxValue, id: \.self) { value in
+            
         }
     }
 }
