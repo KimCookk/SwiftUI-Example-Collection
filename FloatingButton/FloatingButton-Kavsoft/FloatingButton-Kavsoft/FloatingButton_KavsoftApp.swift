ab3e4d8ea9dc4770024e08fca13cfd4430fdbe34
Kim TaeSung <fopc4131@gmail.com>
2024-06-08 16:47:08 +0900

    Floating Button Kavsoft Example Add

---
+++ FloatingButton/FloatingButton-Kavsoft/FloatingButton-Kavsoft/FloatingButton_KavsoftApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingButton_KavsoftApp.swift
+//  FloatingButton-Kavsoft
+//
+//  Created by 김태성 on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct FloatingButton_KavsoftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
