5980689a711ff4c80009b5e233f7850fe66f2964
Kim TaeSung <fopc4131@gmail.com>
2024-06-12 21:28:23 +0900

    DragGesture View - Thinking Example Add

---
+++ DragAndDropView/DragGestureView-Thinking/DragGestureView-Thinking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DragGestureView-Thinking
+//
+//  Created by 김태성 on 6/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DragAndDropView/DragGestureView-Thinking/DragGestureView-Thinking/DragGestureView_ThinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragGestureView_ThinkingApp.swift
+//  DragGestureView-Thinking
+//
+//  Created by 김태성 on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DragGestureView_ThinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
